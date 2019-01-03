df7c2f022c2a21e299cb4369d46acdc7e40fc649
Unknown <gordieiev.artem@gmail.com>
2019-01-03 21:35:27 +0200

    V 1.02a
    Seems to be all is working! (but not tested yet :) )
    + Timer
    + new logo
    + new default sound (15sec)
    - need to refresh all code
    - need to test all cycle of program working

--- Begin.py
+++ Begin.py
@@ -22,14 +22,18 @@ len(fields)
 
 global mute_logo_visible
 
-global time_of_lesson, num_lesson
+global time_of_lesson, num_lesson, times_bell
 global time_bell_tick, time_of_break, times_to, times_from, h, m
 num_lesson = 0
 h = -1
 m = -1
-times_to   = ['09:15:00' , '10:10:00' , '11:20:00' , '12:15:00' , '14:00:00' , '15:00:00']
-times_from = ['10:10:00' , '10:55:00' , '12:05:00' , '13:00:00' , '14:45:00' , '15:45:00']
-times_bell = times_to + times_from
+#times_to   = ['09:15:00' , '10:10:00' , '11:20:00' , '12:15:00' , '14:00:00' , '15:00:00']
+#times_from = ['10:00:00' , '10:55:00' , '12:05:00' , '13:00:00' , '14:45:00' , '19:35:00']
+#times_bell = times_to + times_from
+times_bell = ['09:15:00', '10:00:00', '10:10:00', '10:55:00', '11:20:00', '12:05:00',
+			  '12:15:00', '13:00:00', '14:00:00', '14:45:00', '15:00:00', '15:45:00']
+
+#print(times_bell)
 time_of_lesson = 45 * 60 #seconds of lesson
 time_of_break  = 10 * 60 #seconds of break
 time_bell_tick = time_of_lesson
@@ -41,15 +45,9 @@ default_music = os.getcwd() + "/sound.mp3"
 # playsound("/home/artem/PycharmProjects/SchoolBell/sound.mp3") # Play sound
 
 # Button for mute sound - NOT WORKING NOW!!!
-def buttonMute():
-	global mute_logo_visible
+def playagain():
+	playsound(default_music)
 
-	if mute_logo_visible == True:
-		mute_logo.grid_forget()
-		mute_logo_visible = False
-	else:
-		mute_logo.grid(row=8, column=1, columnspan=1, rowspan=1, sticky="W")
-		mute_logo_visible = True
 
 # Clock and start music function
 def tick():
@@ -57,7 +55,7 @@ def tick():
     global time_bell_tick, time_of_break
     global default_music
     global time_of_lesson
-    global times_to, times_from
+    #global times_to, times_from
     global num_lesson, times_bell, h, m
     # get the current local time from the PC
     time2 = time.strftime('%H:%M:%S')
@@ -73,28 +71,50 @@ def tick():
 	# === Timer
     localtime = time.localtime()
     #time_bell_tick_t = time.strptime(times_bell[num_lesson],'%H:%M:%S')
-    print("num_lesson: " + str(num_lesson))
+    print("num_lesson: {0}".format(str(num_lesson)))
     #m = time_bell_tick_t.tm_min - localtime.tm_min -1
     #h = time_bell_tick_t.tm_hour - localtime.tm_hour - 1
     #print("h: "+str(h) + ", m: " + str(m))
     begin_les = time.strptime(times_bell[0],'%H:%M:%S')
-    begin_les = begin_les.tm_hour
+    #begin_les = begin_les.tm_hour
     end_les   = time.strptime(times_bell[-1],'%H:%M:%S')
-    end_les   = end_les.tm_hour
+    #end_les   = end_les.tm_hour
+
+    #print("begin: {0}, end_les: {1}".format(begin_les, end_les))
+    #print("local hour: {0}, local minutes: {1}, begin_les.tm_hour: {2}, end_les.tm_hour")
+    #print("h: "+str(localtime.tm_hour) + ", m: " + str(begin_les))
 
     #print(str(begin_les) + " " + str(end_les))
+    print(localtime.tm_hour >= begin_les.tm_hour)
+    print(localtime.tm_hour <= end_les.tm_hour)
+    print(localtime.tm_min >= begin_les.tm_min)
+    print(localtime.tm_min <= end_les.tm_min)
 
-    if ( localtime.tm_hour > begin_les ) and ( localtime.tm_hour < end_les ):
-    	time_bell_tick_t = time.strptime(times_bell[num_lesson],'%H:%M:%S')
-    	h = time_bell_tick_t.tm_hour - localtime.tm_hour
-    	m = time_bell_tick_t.tm_min  - localtime.tm_min  - 1
-    	s = 59 - localtime.tm_sec #- time_bell_tick_t.tm_sec
-    	time_to_bell.config(text = str(m) + ":" + str(s) )
+    if (( localtime.tm_hour >= begin_les.tm_hour ) and ( localtime.tm_hour <= end_les.tm_hour )):
+    	print("we are in!")
+    	try:
+    		time_bell_tick_t = time.strptime(times_bell[num_lesson],'%H:%M:%S')
+    		h = time_bell_tick_t.tm_hour - localtime.tm_hour
+    		m = time_bell_tick_t.tm_min  - localtime.tm_min  - 1
+    		s = 59 - localtime.tm_sec #- time_bell_tick_t.tm_sec
+    	except:
+    		print("except")
 
-    	print("localtime: "+str(localtime.tm_min) + ", time to bell: " + str(time_bell_tick_t.tm_hour) + ":" + str(time_bell_tick_t.tm_min))
+    	if num_lesson < len(times_bell):
+    		time_to_bell.config(text = str(m) + ":" + str(s) )
+    	else:
+    		num_lesson = len(times_bell) # making stop to adding lessons
+    		time_to_bell.config(text = "END OF LESSONS!!!")
 
-    	if (h == 0) and (m < 0):
+    	#print("time to bell: {0}:{1}, h:{2}, m:{3}, s:{4} ".format(time_bell_tick_t.tm_hour, time_bell_tick_t.tm_min, h ,m, s))
+    	#except:
+    	#	h = -1
+    	#	m = -1
+    	#	num_lesson = 0
+    	#	time_to_bell.config(text = "END OF LESSONS!!!")
+    	if (h < 0) or (m < 0):
     		num_lesson += 1
+
     else:
     	num_lesson = 0
     	time_to_bell.config(text = "END OF LESSONS!!!")
@@ -165,19 +185,24 @@ if __name__ == '__main__':
 
 	time1 = ''
 	time_bell_tick = time_of_lesson
+	
 	# === Labels ===
 	for i in xrange(0, len(fields)):
-		Label(window, text=fields[i]	  			  , font=("Ubuntu", 8)).grid(row=i+1)
-		Label(window, text=times_to[i][0:5], background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=i+1, column=1)
-		Label(window, text=times_from[i][0:5]  , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=i+1, column=2)
-		
+		Label(window, text=fields[i]	  	   , font=("Ubuntu", 8)).grid(row=i+1)
+	
+	count = 0	
+	for i in range(0, len(times_bell), 2):
+		Label(window, text=times_bell[i][0:5]    , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=count+1, column=1)
+		Label(window, text=times_bell[i+1][0:5]  , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=count+1, column=2)
+		count += 1
+		#print(times_bell[i][0:5], count)
 
 	Label(window, text="Begin:", font=("Ubuntu", 8)).grid(row=0, column=1)
 	Label(window, text="End:"  , font=("Ubuntu", 8)).grid(row=0, column=2)
 
 	button_mute = tk.Button(window,
-								text = "Mute", font=("Ubuntu", 8),
-								command = buttonMute).grid(row=8, column=0)
+								text = "Play again...", font=("Ubuntu", 8),
+								command = playagain).grid(row=8, column=3)
 
 	#tk.Separator(window,orient="vertical").grid(column=3, rowspan=7)
 
@@ -226,11 +251,11 @@ if __name__ == '__main__':
 	#column=5   BELL LOGO
 
 	#bell_img = Image.open("bell-outline.png")
-	bell_photo = PhotoImage(file="bell-outline.png")
+	bell_photo = PhotoImage(file="Photo_logo.png")
 
 	bell_logo = Label(image = bell_photo)
 	bell_logo.image = bell_photo
-	bell_logo.grid(row=1, column=5, columnspan=2, rowspan=3)
+	bell_logo.grid(row=1, column=5, columnspan=2, rowspan=7)
 
 
 
@@ -240,6 +265,19 @@ if __name__ == '__main__':
 	window.mainloop()
 
 
+# === TRASH ===
 
+'''
 
 	
+		Label(window, text=times_to[i][0:5]    , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=i+1, column=1)
+		Label(window, text=times_from[i][0:5]  , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=i+1, column=2)
+
+	global mute_logo_visible
+	if mute_logo_visible == True:
+		mute_logo.grid_forget()
+		mute_logo_visible = False
+	else:
+		mute_logo.grid(row=8, column=1, columnspan=1, rowspan=1, sticky="W")
+		mute_logo_visible = True
+'''
