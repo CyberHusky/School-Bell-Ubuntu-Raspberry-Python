a2cad73aaeb9f1d5a1f661e1f41eb9a6da9abc50
Unknown <gordieiev.artem@gmail.com>
2019-01-04 16:14:01 +0200

    V 1.02
    All basic function should be working!

--- Begin.py
+++ Begin.py
@@ -1,77 +1,223 @@
-import Tkinter as tk
+import sys
+
+if sys.version_info[0] == 3:
+    # for Python3
+    import tkinter as tk
+    from tkinter import filedialog
+    from tkinter import *   ## notice lowercase 't' in tkinter here
+    #from filedialog import askopenfilename 
+else:
+    # for Python2
+    import Tkinter as tk
+    from Tkinter import *   ## notice capitalized T in Tkinter
+    from tkFileDialog import askopenfilename 
+
+#import Tkinter as tk
 import time
 import os
-# Tkinter import Separator, Style
-#from Tkinter import *
-from Tkinter 	  import *
+
+#from Tkinter 	  import *
 from playsound    import playsound
 from PIL 		  import Image
 from time 		  import gmtime, strftime, localtime, strptime
-from tkFileDialog import askopenfilename 
-from threading 	  import Timer
+#from tkFileDialog import askopenfilename 
 
-# ====================================
+
+# ================ WELCOME ====================
 
 begin_ms = "Welcome to School bell app. This app will help you to automatizate your school bell. \n What you need?\n-PC with Ubuntu;\n-Installed Python 3.\nAuthor of app: Gordieiev Artem (gordieiev.artem@gmail.com). V1.0. \n\n"
 print(begin_ms)
 
-# ====================================
+# ================ SETUP ====================
 
 fields = ['Lesson 1:' , 'Lesson 2:', 'Lesson 3:', 'Lesson 4:' , 'Lesson 5:' , 'Lesson 6:']
 len(fields)
 
-global mute_logo_visible
+global times_bell, times_bell_sec
 
-global time_of_lesson, num_lesson, times_bell
-global time_bell_tick, time_of_break, times_to, times_from, h, m
-num_lesson = 0
-h = -1
-m = -1
-#times_to   = ['09:15:00' , '10:10:00' , '11:20:00' , '12:15:00' , '14:00:00' , '15:00:00']
-#times_from = ['10:00:00' , '10:55:00' , '12:05:00' , '13:00:00' , '14:45:00' , '19:35:00']
-#times_bell = times_to + times_from
+# === Times for bell ===
 times_bell = ['09:15:00', '10:00:00', '10:10:00', '10:55:00', '11:20:00', '12:05:00',
 			  '12:15:00', '13:00:00', '14:00:00', '14:45:00', '15:00:00', '15:45:00']
+	# Make array with bell times in seconds
+times_bell_sec=[]
+for i in range(0, len(times_bell)):
+	bell_time = time.strptime(times_bell[i],'%H:%M:%S')
+	times_bell_sec.append(bell_time.tm_hour * 3600 + bell_time.tm_min * 60 + bell_time.tm_sec)
 
-#print(times_bell)
-time_of_lesson = 45 * 60 #seconds of lesson
-time_of_break  = 10 * 60 #seconds of break
-time_bell_tick = time_of_lesson
-
+# === Music setup ===
 global default_music 
 default_music = os.getcwd() + "/sound.mp3"
-#print os.getcwd()
-#print default_music
+# YOU SHOULD CHOOSE FILE WITHOUT SPACES IN NAME!!!
 # playsound("/home/artem/PycharmProjects/SchoolBell/sound.mp3") # Play sound
 
-# Button for mute sound - NOT WORKING NOW!!!
+# ====================================
+
+
+# === BUTTON FUNCTIONS ===
+
+# Button play music again
 def playagain():
 	playsound(default_music)
 
 
-# Clock and start music function
+
+# === FUNCTIONS ===
+
+def time2sec(time_arg):
+	timeis = time.strptime(time_arg,'%H:%M:%S')
+	timeinsec = timeis.tm_hour * 3600 + timeis.tm_min * 60 + timeis.tm_sec
+	return timeinsec
+
+# === Clock function + play music
 def tick():
     global time1
-    global time_bell_tick, time_of_break
     global default_music
-    global time_of_lesson
-    #global times_to, times_from
-    global num_lesson, times_bell, h, m
+    global times_bell, times_bell_sec
     # get the current local time from the PC
     time2 = time.strftime('%H:%M:%S')
     # if time string has changed, update it
     if time2 != time1:
         time1 = time2
-        clock.config(text=time2)
+        lable_clock.config(text=time2)
         # calls itself every 200 milliseconds
 		# to update the time display as needed
 		# could use >200 ms, but display gets jerky
-    clock.after(200, tick)
+    lable_clock.after(200, tick)
 	
 	# === Timer
     localtime = time.localtime()
-    #time_bell_tick_t = time.strptime(times_bell[num_lesson],'%H:%M:%S')
-    print("num_lesson: {0}".format(str(num_lesson)))
+    localtime_sec = time2sec("{0}:{1}:{2}".format(localtime.tm_hour, localtime.tm_min, localtime.tm_sec))
+
+    if (localtime_sec < times_bell_sec[0]) or (localtime_sec > times_bell_sec[-1]):
+    	lable_belltime.config(text = "END OF LESSONS!!!")
+    else:
+    	for i in range(0, len(times_bell_sec)-1):
+    		if (localtime_sec >= times_bell_sec[i]) and (localtime_sec <= times_bell_sec[i+1]):
+    			sec = times_bell_sec[i+1] - localtime_sec
+    			s = sec % 60
+    			m = int(sec / 60)
+    			lable_belltime.config(text = "{0}:{1}".format(m, s) )
+
+    # === MUSIC PLAY
+    for i in range(0, len(times_bell)):
+    	if (time2 == times_bell[i]) or (time2 == times_bell[i]):
+    		playsound(default_music)
+# === END of Clock function
+
+
+# Open file 
+def file_open():
+	global default_music
+	default_music = askopenfilename()
+	entry_file.insert(END, default_music)
+	print (default_music)
+
+if __name__ == '__main__':
+
+	#global default_music # directory of music file
+
+	# === Window ===
+	window = tk.Tk()
+	window.title('SchoolBell. V1.0a')
+	window.resizable(0, 0)
+	#window.geometry('500x200')
+
+	time1 = ''
+	
+	# === Labels ===
+		# Make labels with lesson numbers
+	for i in range(0, len(fields)):
+		Label(window, text=fields[i], font=("Ubuntu", 8)).grid(row=i+1)
+	
+		# Make lessons time labels
+	count = 0	
+	for i in range(0, len(times_bell), 2):
+		Label(window, text=times_bell[i][0:5]    , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=count+1, column=1)
+		Label(window, text=times_bell[i+1][0:5]  , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=count+1, column=2)
+		count += 1
+		
+		# Make lables with Begin and End word
+	Label(window, text="Begin:", font=("Ubuntu", 8)).grid(row=0, column=1)
+	Label(window, text="End:"  , font=("Ubuntu", 8)).grid(row=0, column=2)
+
+
+
+		# Make label for Time now	
+	Label(window, text="TIME NOW:", font=("Ubuntu", 14)).grid(row=2, column=3)
+	lable_clock = Label(window, background="#F23A3A", font=("Ubuntu", 14))
+	lable_clock.grid(row=3, column=3)
+	
+	
+
+	
+	Label(window, text = "TIME TO BELL:", font=("Ubuntu", 14)).grid(row=4, column=3)
+	lable_belltime = Label(window, background="#F23A3A")
+	lable_belltime.grid(row=5, column=3)
+	lable_belltime.config(text = "???")
+
+	tick()
+	
+
+
+	# === FILE buttons + entry + play again
+	button_file = Button(window, font=("Ubuntu", 8),
+								 text = "FILE...",
+								 command = file_open)
+	button_file.grid(row=7, column=0)
+
+	entry_file = Entry(window)
+	entry_file.grid(row=7, column=1, columnspan=2)
+	entry_file.insert(END, default_music)
+
+	tk.Button(window,
+					text = "Play again...", font=("Ubuntu", 8),
+					command = playagain).grid(row=7, column=3)
+
+
+	# === Bell LOGO ===
+
+	bell_photo = PhotoImage(file="Photo_logo.png")
+
+	label_logo = Label(image = bell_photo)
+	label_logo.image = bell_photo
+	label_logo.grid(row=1, column=5, columnspan=2, rowspan=7)
+
+
+
+
+
+
+	window.mainloop()
+
+
+# === TRASH ===
+
+'''
+
+
+
+	#from threading 	  import Timer
+	
+
+
+	mute_photo = PhotoImage(file="volume-off-indicator.png")
+	mute_logo = Label(image = mute_photo)
+	mute_logo.image = mute_photo
+	mute_logo.grid(row=8, column=1, columnspan=1, rowspan=1, sticky="W")
+	mute_logo.grid_forget()
+	mute_logo_visible = False
+
+
+
+
+
+
+
+
+
+    localtime = time.localtime()
+    #time_bell_tick_t = time.strptime(times_bell[num_bell],'%H:%M:%S')
+    print("num_bell: {0}".format(str(num_bell)))
     #m = time_bell_tick_t.tm_min - localtime.tm_min -1
     #h = time_bell_tick_t.tm_hour - localtime.tm_hour - 1
     #print("h: "+str(h) + ", m: " + str(m))
@@ -85,10 +231,17 @@ def tick():
     #print("h: "+str(localtime.tm_hour) + ", m: " + str(begin_les))
 
     #print(str(begin_les) + " " + str(end_les))
-    print(localtime.tm_hour >= begin_les.tm_hour)
-    print(localtime.tm_hour <= end_les.tm_hour)
-    print(localtime.tm_min >= begin_les.tm_min)
-    print(localtime.tm_min <= end_les.tm_min)
+    #print(localtime.tm_hour >= begin_les.tm_hour)
+    #print(localtime.tm_hour <= end_les.tm_hour)
+    #print(localtime.tm_min >= begin_les.tm_min)
+    #print(localtime.tm_min <= end_les.tm_min)
+
+
+
+#print(localtime.tm_hour >= begin_les.tm_hour)
+    #print(localtime.tm_hour <= end_les.tm_hour)
+    #print(localtime.tm_min >= begin_les.tm_min)
+    #print(localtime.tm_min <= end_les.tm_min)
 
     if (( localtime.tm_hour >= begin_les.tm_hour ) and ( localtime.tm_hour <= end_les.tm_hour )):
     	print("we are in!")
@@ -112,162 +265,18 @@ def tick():
     	#	m = -1
     	#	num_lesson = 0
     	#	time_to_bell.config(text = "END OF LESSONS!!!")
-    	if (h < 0) or (m < 0):
+    	if (h <= 0) or (m < 0):
     		num_lesson += 1
 
     else:
     	num_lesson = 0
     	time_to_bell.config(text = "END OF LESSONS!!!")
 
-    # === MUSIC PLAY
-    for i in xrange(0, len(times_bell)):
-    	if (time2 == times_bell[i]) or (time2 == times_bell[i]):
-    		playsound(default_music)
-
-'''
-    if (num_lesson > len(times_bell)):
-    	time_to_bell.config(text = "END OF LESSONS!!!")
-    	num_lesson = 0
-    elif (h < 0):
-    	num_lesson += 1
-    	localtime = time.localtime()
-    	time_bell_tick_t = time.strptime(times_bell[num_lesson],'%H:%M:%S')
-    	m = time_bell_tick_t.tm_min - localtime.tm_min - 1
-    	h = time_bell_tick_t.tm_hour - localtime.tm_hour 
-    	s = 59 - localtime.tm_sec #- time_bell_tick_t.tm_sec
-    	#time_to_bell.config(text = str(m) + ":" + str(s) )
-    	if (m < 0):
-    		num_lesson += 1
-    		localtime = time.localtime()
-    		time_bell_tick_t = time.strptime(times_bell[num_lesson],'%H:%M:%S')
-    		m = time_bell_tick_t.tm_min - localtime.tm_min - 1
-    		h = time_bell_tick_t.tm_hour - localtime.tm_hour - 1
-    		s = 59 - localtime.tm_sec #- time_bell_tick_t.tm_sec
-    	else:
-    		time_to_bell.config(text = str(m) + ":" + str(s) )
-'''    
-    	
-
-
-    		
-
-def bell():
-	time_to_bell.after(1000, bell)
-	global time_bell_tick
-	#time_bell_tick -= 1
-	#m = time_bell_tick // 60
-	#s = time_bell_tick-m*60
-	#time_to_bell.config(text = str(m) + ":" + str(s) )
-    
-
-
-def timeout():
-	print("BELLING")
-	time_to_bell.config(text = "???")
-
-# Open file 
-def file_open():
-	global default_music
-	default_music = askopenfilename()
-	file_entry.insert(END, default_music)
-	print default_music
-
-if __name__ == '__main__':
-
-	global default_music # directory of file
-	global time_bell_tick
-
-	# === Window ===
-	window = tk.Tk()
-	window.title('SchoolBell. V1.0a')
-	window.resizable(0, 0)
-	#window.geometry('500x200')
-
-	time1 = ''
-	time_bell_tick = time_of_lesson
-	
-	# === Labels ===
-	for i in xrange(0, len(fields)):
-		Label(window, text=fields[i]	  	   , font=("Ubuntu", 8)).grid(row=i+1)
-	
-	count = 0	
-	for i in range(0, len(times_bell), 2):
-		Label(window, text=times_bell[i][0:5]    , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=count+1, column=1)
-		Label(window, text=times_bell[i+1][0:5]  , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=count+1, column=2)
-		count += 1
-		#print(times_bell[i][0:5], count)
-
-	Label(window, text="Begin:", font=("Ubuntu", 8)).grid(row=0, column=1)
-	Label(window, text="End:"  , font=("Ubuntu", 8)).grid(row=0, column=2)
-
-	button_mute = tk.Button(window,
-								text = "Play again...", font=("Ubuntu", 8),
-								command = playagain).grid(row=8, column=3)
-
-	#tk.Separator(window,orient="vertical").grid(column=3, rowspan=7)
-
-	#column = 3
-	
-	#localtime = time.asctime( time.localtime(time.time()) )
-	#print "Local time is:", localtime.tm_year
-	print "Local time is:", strftime("%X")
-
-	file_entry = Entry(window)
-	file_entry.grid(row=1, column=3)
-	file_entry.insert(END, default_music)
-	
-	Label(window, text="TIME NOW:", font=("Ubuntu", 14)).grid(row=2, column=3)
-	#time_now.configure(text = strftime("%X"))
-	clock = Label(window, background="#F23A3A", font=("Ubuntu", 14))
-	clock.grid(row=3, column=3)
-	
-	
-
-	
-	Label(window, text = "TIME TO BELL:", font=("Ubuntu", 14)).grid(row=4, column=3)
-	time_to_bell = Label(window, background="#F23A3A")
-	time_to_bell.grid(row=5, column=3)
-	time_to_bell.config(text = "???")
-
-	tick()
-	bell()
-
-	mute_photo = PhotoImage(file="volume-off-indicator.png")
-	mute_logo = Label(image = mute_photo)
-	mute_logo.image = mute_photo
-	mute_logo.grid(row=8, column=1, columnspan=1, rowspan=1, sticky="W")
-	mute_logo.grid_forget()
-	mute_logo_visible = False
-	
-	#
-
-	#column = 4
-	button_file = Button(window, font=("Ubuntu", 8),
-								 text = "FILE...",
-								 command = file_open)
-	button_file.grid(row=1, column=4)
-
-
-	#column=5   BELL LOGO
-
-	#bell_img = Image.open("bell-outline.png")
-	bell_photo = PhotoImage(file="Photo_logo.png")
-
-	bell_logo = Label(image = bell_photo)
-	bell_logo.image = bell_photo
-	bell_logo.grid(row=1, column=5, columnspan=2, rowspan=7)
 
 
 
 
 
-
-	window.mainloop()
-
-
-# === TRASH ===
-
-'''
 
 	
 		Label(window, text=times_to[i][0:5]    , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=i+1, column=1)
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-SchoolBell
+# SchoolBell
+
+School bell in Python (with GUI - Tkinter + .grid) that can play mp3 music at the time and have Timer. You could to change time for bell and music. Insulator for Linux_x64 version. If you make Windows installer (or EXE) it would be helpful for me!
+Python version: 2.7.12
+
+## Getting Started
+
+For develop I used next libraries:
+```
+apt-get install python-tk python3-tk tk-dev
+pip install --user pillow
+pip install --user Image
+
+pip install pyinstaller
+```
+
+## Features
+
+What Schoolbell program can do:
+- Show current time;
+- Show time for nearest bell;
+- Play music on the chosen time (you could change time only in .py file, not in executable file);
+- Play music when you need
+- You could choose music for play (default: sound.mp3)
+
+Setup in source file:
+- You could choose time for bell in your school. Program automatically should to adapts to your setup;
+- You could choose logo of your school;
+- You could choose number of lessons.
+
+
+## Authors
+Gordieiev Artem. For donation or questions you could write me on email: gordieiev.artem@gmail.com
+
+It would be awesome if you help me to make this program better (make a different language, make a setup function in GUI)
+
