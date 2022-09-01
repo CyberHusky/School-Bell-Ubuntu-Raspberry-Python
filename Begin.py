9192b0b7c00662c0e35ad7ac0f91e0fc57a2877a
Gordieiev Artem <gordieiev.artem@gmail.com>
2022-09-01 10:23:15 +0300

    V 1.03
    Start working on Air Alert

--- Begin.py
+++ Begin.py
@@ -4,24 +4,57 @@ if sys.version_info[0] == 3:
     # for Python3
     import tkinter as tk
     from tkinter import filedialog
-    from tkinter import *   ## notice lowercase 't' in tkinter here
-    #from filedialog import askopenfilename 
+    from tkinter import *  ## notice lowercase 't' in tkinter here
+	# from filedialog import askopenfilename
 else:
     # for Python2
     import Tkinter as tk
-    from Tkinter import *   ## notice capitalized T in Tkinter
-    from tkFileDialog import askopenfilename 
+    from Tkinter import *  ## notice capitalized T in Tkinter
+    from tkFileDialog import askopenfilename
 
-#import Tkinter as tk
+# import Tkinter as tk
 import time
 import os
 
-#from Tkinter 	  import *
-from playsound    import playsound
-from PIL 		  import Image
-from time 		  import gmtime, strftime, localtime, strptime
-#from tkFileDialog import askopenfilename 
+# from Tkinter 	  import *
+from playsound import playsound
+from PIL import Image
+from time import gmtime, strftime, localtime, strptime
+# from tkFileDialog import askopenfilename
 
+# ApiKey
+import csv
+import codecs
+import requests
+import urllib.request
+import urllib.error
+
+
+
+import requests
+import pprint
+from requests.structures import CaseInsensitiveDict
+
+url = "https://api.ukrainealarm.com/api/v3/alerts"
+
+headers = CaseInsensitiveDict()
+headers["accept"] = "application/json"
+headers["Authorization"] = "086c31f0:fab40d26ccec9c6deaa47265b753fdc5"
+
+resp = requests.get(url, headers=headers)
+data = resp.json()
+
+#print(data["regionId"])
+
+for i in data:
+    print(i.items())
+    if i.keys() == 'regionId':
+        print(i.values())
+
+#print(resp.json())
+
+
+'''
 
 # ================ WELCOME ====================
 
@@ -30,28 +63,28 @@ print(begin_ms)
 
 # ================ SETUP ====================
 
-fields = ['Lesson 1:' , 'Lesson 2:', 'Lesson 3:', 'Lesson 4:' , 'Lesson 5:' , 'Lesson 6:', 'Lesson 7:', 'Lesson 8:', 'Lesson 9:', 'Lesson 10:', 'Lesson 11:']
+fields = ['Lesson 1:', 'Lesson 2:', 'Lesson 3:', 'Lesson 4:', 'Lesson 5:', 'Lesson 6:', 'Lesson 7:', 'Lesson 8:',
+          'Lesson 9:', 'Lesson 10:', 'Lesson 11:']
 len(fields)
 
 global times_bell, times_bell_sec
 
 # === Times for bell ===
-times_bell = ['08:30:00','09:05:00','09:15:00', '10:00:00', '10:10:00', '10:55:00', '11:10:00', '11:55:00', '12:15:00', '13:00:00',
-			  '13:45:00', '14:30:00', '14:40:00', '15:25:00', '15:35:00', '16:20:00', '16:40:00', '17:25:00', '17:35:00', '18:20:00', '18:30:00', '19:00:00']
+times_bell = ['08:30:00', '09:05:00', '09:15:00', '10:00:00', '10:10:00', '10:55:00', '11:10:00', '11:55:00',
+              '12:15:00', '13:00:00',
+              '13:45:00', '14:30:00', '14:40:00', '15:25:00', '15:35:00', '16:20:00', '16:40:00', '17:25:00',
+              '17:35:00', '18:20:00', '18:30:00', '19:00:00']
 
-'''
-times_bell = ['08:30:00','09:05:00','09:15:00', '10:00:00', '10:20:00', '11:05:00', '11:25:00', '12:10:00', '12:30:00', '13:15:00',
-			  '14:15:00', '15:00:00', '15:15:00', '16:00:00', '16:30:00', '17:15:00', '17:30:00', '18:15:00', '18:30:00', '19:00:00']
-'''
 
-	# Make array with bell times in seconds
-times_bell_sec=[]
+
+# Make array with bell times in seconds
+times_bell_sec = []
 for i in range(0, len(times_bell)):
-	bell_time = time.strptime(times_bell[i],'%H:%M:%S')
-	times_bell_sec.append(bell_time.tm_hour * 3600 + bell_time.tm_min * 60 + bell_time.tm_sec)
+    bell_time = time.strptime(times_bell[i], '%H:%M:%S')
+    times_bell_sec.append(bell_time.tm_hour * 3600 + bell_time.tm_min * 60 + bell_time.tm_sec)
 
 # === Music setup ===
-global default_music 
+global default_music
 default_music = os.getcwd() + "/sound.mp3"
 
 
@@ -73,16 +106,16 @@ default_music = os.getcwd() + "/sound.mp3"
 
 # Button play music again
 def playagain():
-	playsound(default_music)
-
+    playsound(default_music)
 
 
 # === FUNCTIONS ===
 
 def time2sec(time_arg):
-	timeis = time.strptime(time_arg,'%H:%M:%S')
-	timeinsec = timeis.tm_hour * 3600 + timeis.tm_min * 60 + timeis.tm_sec
-	return timeinsec
+    timeis = time.strptime(time_arg, '%H:%M:%S')
+    timeinsec = timeis.tm_hour * 3600 + timeis.tm_min * 60 + timeis.tm_sec
+    return timeinsec
+
 
 # === Clock function + play music
 def tick():
@@ -96,115 +129,113 @@ def tick():
         time1 = time2
         lable_clock.config(text=time2)
         # calls itself every 200 milliseconds
-		# to update the time display as needed
-		# could use >200 ms, but display gets jerky
+    # to update the time display as needed
+    # could use >200 ms, but display gets jerky
     lable_clock.after(200, tick)
-	
-	# === Timer
+
+    # === Timer
     localtime = time.localtime()
     localtime_sec = time2sec("{0}:{1}:{2}".format(localtime.tm_hour, localtime.tm_min, localtime.tm_sec))
 
     if (localtime_sec < times_bell_sec[0]) or (localtime_sec > times_bell_sec[-1]):
-    	lable_belltime.config(text = "END OF LESSONS!!!")
+        lable_belltime.config(text="END OF LESSONS!!!")
     else:
-    	for i in range(0, len(times_bell_sec)-1):
-    		if (localtime_sec >= times_bell_sec[i]) and (localtime_sec <= times_bell_sec[i+1]):
-    			sec = times_bell_sec[i+1] - localtime_sec
-    			s = sec % 60
-    			m = int(sec / 60)
-    			lable_belltime.config(text = "{0}:{1}".format(m, s) )
+        for i in range(0, len(times_bell_sec) - 1):
+            if (localtime_sec >= times_bell_sec[i]) and (localtime_sec <= times_bell_sec[i + 1]):
+                sec = times_bell_sec[i + 1] - localtime_sec
+                s = sec % 60
+                m = int(sec / 60)
+                lable_belltime.config(text="{0}:{1}".format(m, s))
 
     # === MUSIC PLAY
     for i in range(0, len(times_bell)):
-    	if (time2 == times_bell[i]) or (time2 == times_bell[i]):
-    		playsound(default_music)
+        if (time2 == times_bell[i]) or (time2 == times_bell[i]):
+            playsound(default_music)
+
+
 # === END of Clock function
 
 
 # Open file 
 def file_open():
-	global default_music
-	default_music = askopenfilename()
-	entry_file.insert(END, default_music)
-	print (default_music)
+    global default_music
+    default_music = askopenfilename()
+    entry_file.insert(END, default_music)
+    print(default_music)
+
 
 if __name__ == '__main__':
 
-	#global default_music # directory of music file
+    # global default_music # directory of music file
 
-	# === Window ===
-	window = tk.Tk()
-	window.title('SchoolBell. V1.0a')
-	window.resizable(0, 0)
-	#window.geometry('500x200')
+    # === Window ===
+    window = tk.Tk()
+    window.title('SchoolBell. V1.0a')
+    window.resizable(0, 0)
+    # window.geometry('500x200')
 
-	time1 = ''
-	
-	# === Labels ===
-		# Make labels with lesson numbers
-	for i in range(0, len(fields)):
-		Label(window, text=fields[i], font=("Ubuntu", 8)).grid(row=i+1)
-	
-		# Make lessons time labels
-	count = 0	
-	for i in range(0, len(times_bell), 2):
-		Label(window, text=times_bell[i][0:5]    , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=count+1, column=1)
-		Label(window, text=times_bell[i+1][0:5]  , background="lightblue", width = 10, font=("Ubuntu", 8)).grid(row=count+1, column=2)
-		count += 1
-		
-		# Make lables with Begin and End word
-	Label(window, text="Begin:", font=("Ubuntu", 8)).grid(row=0, column=1)
-	Label(window, text="End:"  , font=("Ubuntu", 8)).grid(row=0, column=2)
+    time1 = ''
 
+    # === Labels ===
+    # Make labels with lesson numbers
+    for i in range(0, len(fields)):
+        Label(window, text=fields[i], font=("Ubuntu", 8)).grid(row=i + 1)
 
+    # Make lessons time labels
+    count = 0
+    for i in range(0, len(times_bell), 2):
+        Label(window, text=times_bell[i][0:5], background="lightblue", width=10, font=("Ubuntu", 8)).grid(row=count + 1,
+                                                                                                          column=1)
+        Label(window, text=times_bell[i + 1][0:5], background="lightblue", width=10, font=("Ubuntu", 8)).grid(
+            row=count + 1, column=2)
+        count += 1
 
-		# Make label for Time now	
-	Label(window, text="TIME NOW:", font=("Ubuntu", 14)).grid(row=2, column=3)
-	lable_clock = Label(window, background="#F23A3A", font=("Ubuntu", 14))
-	lable_clock.grid(row=3, column=3)
-	
-	
+    # Make lables with Begin and End word
+    Label(window, text="Begin:", font=("Ubuntu", 8)).grid(row=0, column=1)
+    Label(window, text="End:", font=("Ubuntu", 8)).grid(row=0, column=2)
 
-	
-	Label(window, text = "TIME TO BELL:", font=("Ubuntu", 14)).grid(row=4, column=3)
-	lable_belltime = Label(window, background="#F23A3A")
-	lable_belltime.grid(row=5, column=3)
-	lable_belltime.config(text = "???")
+    # Make label for Time now
+    Label(window, text="TIME NOW:", font=("Ubuntu", 14)).grid(row=2, column=3)
+    lable_clock = Label(window, background="#F23A3A", font=("Ubuntu", 14))
+    lable_clock.grid(row=3, column=3)
 
-	tick()
-	
+    Label(window, text="TIME TO BELL:", font=("Ubuntu", 14)).grid(row=4, column=3)
+    lable_belltime = Label(window, background="#F23A3A")
+    lable_belltime.grid(row=5, column=3)
+    lable_belltime.config(text="???")
 
+    tick()
 
-	# === FILE buttons + entry + play again
-	button_file = Button(window, font=("Ubuntu", 8),
-								 text = "FILE...",
-								 command = file_open)
-	button_file.grid(row=count+1, column=0)
+    # === FILE buttons + entry + play again
+    button_file = Button(window, font=("Ubuntu", 8),
+                         text="FILE...",
+                         command=file_open)
+    button_file.grid(row=count + 1, column=0)
 
-	entry_file = Entry(window)
-	entry_file.grid(row=count+1, column=1, columnspan=2)
-	entry_file.insert(END, default_music)
+    entry_file = Entry(window)
+    entry_file.grid(row=count + 1, column=1, columnspan=2)
+    entry_file.insert(END, default_music)
 
-	tk.Button(window,
-					text = "Play again...", font=("Ubuntu", 8),
-					command = playagain).grid(row=count+1, column=3)
+    tk.Button(window,
+              text="Play again...", font=("Ubuntu", 8),
+              command=playagain).grid(row=count + 1, column=3)
 
+    # === Bell LOGO ===
 
-	# === Bell LOGO ===
+    bell_photo = PhotoImage(file="Photo_logo.png")
 
-	bell_photo = PhotoImage(file="Photo_logo.png")
+    label_logo = Label(image=bell_photo)
+    label_logo.image = bell_photo
+    label_logo.grid(row=1, column=5, columnspan=2, rowspan=7)
 
-	label_logo = Label(image = bell_photo)
-	label_logo.image = bell_photo
-	label_logo.grid(row=1, column=5, columnspan=2, rowspan=7)
+    window.mainloop()
 
-
-
-
-
-
-	window.mainloop()
+'''
 
 
 # === TRASH ===
 
+'''
+times_bell = ['08:30:00','09:05:00','09:15:00', '10:00:00', '10:20:00', '11:05:00', '11:25:00', '12:10:00', '12:30:00', '13:15:00',
+			  '14:15:00', '15:00:00', '15:15:00', '16:00:00', '16:30:00', '17:15:00', '17:30:00', '18:15:00', '18:30:00', '19:00:00']
+'''
